04f972720387f6f20281d281b6d57c9f92466197
Jai-austin95 <jai.austin95@gmail.com>
2020-03-15 10:27:12 +0000

    added timeOut to the flipped non matching card in order to see them

--- scripts.js
+++ scripts.js
@@ -10,25 +10,35 @@ function flipCard(){
     if (!hasFlippedCard) {
 
         // First click
+
         hasFlippedCard = true;
         firstCard = this;
 
     } else {
 
         // Second click
+
         hasFlippedCard = false;
         secondCard = this;
         
         // Do cards match?
 
         if (firstCard.dataset.image === secondCard.dataset.image) {
+            
             // a match!
+
             firstCard.removeEventListener("click", flipCard);
             secondCard.removeEventListener("click", flipCard);
-       }
 
-       console.log("function has been executed.")
-    }
+       } else {
+
+        // its not a match! set a timer to be able to see both cards flipping
+
+        setTimeout(() => {
+        firstCard.classList.remove("flip");
+        SecondCard.classList.remove("flip");
+    }, 1500);
+  }  
 }
 cards.forEach(card => card.addEventListener("click", flipCard))
 ;
