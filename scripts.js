0cf8251265e333da8f7bbd07d0cc218cb5cf70d7
Jai-austin95 <jai.austin95@gmail.com>
2020-03-15 10:18:09 +0000

    added the beggning js for matching the cards together and stopping the event listener to remove ability to flip cards

--- scripts.js
+++ scripts.js
@@ -1,8 +1,34 @@
 const cards = document.querySelectorAll(".memory-card");
 
-function flipCard(){
-    this.classList.toggle("flip");
-}
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+function flipCard(){
+
+    this.classList.toggle("flip");
+
+    if (!hasFlippedCard) {
+
+        // First click
+        hasFlippedCard = true;
+        firstCard = this;
+
+    } else {
+
+        // Second click
+        hasFlippedCard = false;
+        secondCard = this;
+        
+        // Do cards match?
+
+        if (firstCard.dataset.image === secondCard.dataset.image) {
+            // a match!
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard);
+       }
+
+       console.log("function has been executed.")
+    }
+}
 cards.forEach(card => card.addEventListener("click", flipCard))
 ;
